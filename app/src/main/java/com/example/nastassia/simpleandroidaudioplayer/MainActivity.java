681adafb0640ccf7a965d6ac6f9112e2bc1a98cc
Yauheni Liohenki <legenkiyes@gmail.com>
2016-04-26 00:16:32 +0300

    choose from 2 variants
    choose from 2 variants

--- app/src/main/java/com/example/nastassia/simpleandroidaudioplayer/MainActivity.java
+++ app/src/main/java/com/example/nastassia/simpleandroidaudioplayer/MainActivity.java
@@ -3,15 +3,44 @@ package com.example.nastassia.simpleandroidaudioplayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static MediaPlayer mp = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.someprodigy);
-        mp.start();
+        final ListView list = (ListView)findViewById(R.id.listView);
+        final MainActivity that = this;
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (mp != null)
+                    mp.stop();
+                mp = null;
+                switch (position)
+                {
+                    case 0:
+                        mp = MediaPlayer.create(that, R.raw.someprodigy);
+                        break;
+                    case 1:
+                        mp = MediaPlayer.create(that, R.raw.boxcat);
+                        break;
+                    default:
+                        break;
+                }
+                if (mp != null)
+                    mp.start();
+            }
+        });
+
+
+
     }
 }
